e2868983a7fa92f13e12421e8ef5b55ca1ee022c
Madis Abel <mabel@twilio.com>
2019-04-01 17:45:32 +0300

    Use version 2.0.0 of @twilio/flex-webchat-ui

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// Uncomment to add polyfills to run on IE (e.g. IE11)
+// import 'react-app-polyfill/ie9';
+// import 'ie-array-find-polyfill';
+// import 'array-findindex-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'regenerator-runtime/runtime';
